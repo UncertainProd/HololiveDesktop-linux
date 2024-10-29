4e258fffbd9d7f4b1d78e863bca24974f0b29641
NisenoShitsu <faketoothache123@gmail.com>
2024-10-29 22:17:23 +0800

    Source Code
    10-29-2024

--- Hololive EN Myth (First Gen)/target/Shimeji-ee_1.0.9_Mischievous/add_to_startup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set TARGET=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Shimeji-ee.lnk
-set SOURCE=%~dp0Shimeji-ee.exe
-if not exist "%SOURCE%" (
-    echo Shimeji-ee.exe not found in %~dp0
-    exit /b
-)
-echo Creating shortcut to Shimeji-ee in Startup folder...
-powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut('%TARGET%');$s.TargetPath='%SOURCE%';$s.Save()"
--- Hololive EN Myth (First Gen)/target/Shimeji-ee_1.0.9_Mischievous/remove_from_startup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set TARGET=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Shimeji-ee.lnk
-if exist "%TARGET%" (
-    echo Removing Shimeji-ee from Startup folder...
-    del "%TARGET%"
-) else (
-    echo Shortcut not found in Startup folder.
-)
