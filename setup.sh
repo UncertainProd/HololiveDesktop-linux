23a7ab816ba6c29c4a02a703bae707cf90f61ae7
UncertainProd <83609901+UncertainProd@users.noreply.github.com>
2025-01-20 00:54:03 +0545

    added some logging to the setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+echo "Pulling JNA version 3.4.0 from git... (from: https://github.com/java-native-access/jna.git)"
 # pull jna version 3.4.0 (git submodules don't work with tags, but git clone does :/)
 git clone --depth 1 --branch 3.4.0 https://github.com/java-native-access/jna.git jna_3_4_0_src
 
+echo "Pulling the netbeans package from https://github.com/apache/netbeans.git ..."
 # cloning this cuz there's the awtextras package with AbosulteLayout.java inside it
 git clone --depth 1 https://github.com/apache/netbeans.git
 
+echo "Downloading nimrod from https://nilogonzalez.es/nimrodlf/data/nimrodlf-src.zip ..."
 # the nimrod library does not exist on git (afaik), but we download it in zip form like so:
 wget https://nilogonzalez.es/nimrodlf/data/nimrodlf-src.zip
 unzip nimrodlf-src.zip -d nimrodlf-src
 rm -rf nimrodlf-src.zip
 
+echo "Setup complete. You should be ready to build now"
